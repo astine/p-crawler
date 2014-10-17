306caf1c421dfc973d49861611b24df8d552a5eb
Andrew Stine <stine.drew@gmail.com>
2014-10-17 17:43:06 -0400

    Added basic crawler

---
+++ src/p_crawler/core.clj
@@ -0,0 +1,125 @@
+(ns p-crawler.core
+  "This is the primary ."
+  (:gen-class)
+  (:require [clojure.core.async :refer [thread onto-chan chan >!! <!!]]
+            [monger.core :as mg]
+            [net.cgrand.enlive-html :as html]
+            [clojurewerkz.urly.core :as url]
+            [taoensso.timbre :as logger]
+            [clojure.string :refer [split]]
+            [clj-http.client :as http]
+            [clj-robots.core :as robots]
+            [clj-time.core :as time]
+            [clj-time.coerce :as coerce-time])
+  (:import [java.net URL UnknownHostException]))
+
+(logger/refer-timbre)
+
+(def uri-queue (chan 10000))
+
+(def domains (atom {}))
+
+(def robots-expire-duration (time/days 1))
+
+(def default-crawl-delay (time/seconds 30))
+
+(def connection-manager (clj-http.conn-mgr/make-socks-proxied-conn-manager "localhost" 8080))
+
+(def connection-defaults {:connection-manager connection-manager
+                          :conn-timeout 1000
+                          :socket-timeout 1000
+                          :throw-exceptions false
+                          :follow-redirects false})
+
+(defn update-domain! [domain key value]
+  (get-in (swap! domains assoc-in [domain key] value)
+          [domain key]))
+
+(defn get-domain-value [domain key]
+  (get-in @domains [domain key]))
+
+(defn fetch-robots [domain]
+  (try
+    (let [{:keys [body] :as response}
+          (http/get (str "http://" domain "/robots.txt")
+                    connection-defaults)]
+      (robots/parse body))
+    (catch Exception e
+      (error e)
+      nil)))
+
+(defn robots [domain]
+  (let [robots (get-domain-value domain :robots)]
+    (if (or (not robots)
+            (time/before? (coerce-time/from-long (:modified-time robots))
+                          (time/minus (time/now) robots-expire-duration)))
+      (update-domain! domain :robots (fetch-robots domain))
+      robots)))
+
+(defn extract-domain-from-url [url]
+  (when url
+    (let [[[match protocol domain path]]
+          (re-seq #"^(\w*://)?([^:_/]+\.\w+)([:/].*)?" url)
+          tld (when domain (last (split domain #"\.")))]
+      (when (#{"ac" "ad" "ae" "aero" "af" "ag" "ai" "al" "am" "an" "ao" "aq" "ar" "arpa" "as" "asia" "at" "au" "aw" "ax" "az" "ba" "bb" "bd" "be" "bf" "bg" "bh" "bi" "biz" "bj" "bm" "bn" "bo" "br" "bs" "bt" "bv" "bw" "by" "bz" "ca" "cat" "cc" "cd" "cf" "cg" "ch" "ci" "ck" "cl" "cm" "cn" "co" "com" "coop" "cr" "cu" "cv" "cw" "cx" "cy" "cz" "de" "dj" "dk" "dm" "do" "dz" "ec" "edu" "ee" "eg" "er" "es" "et" "eu" "fi" "fj" "fk" "fm" "fo" "fr" "ga" "gb" "gd" "ge" "gf" "gg" "gh" "gi" "gl" "gm" "gn" "gov" "gp" "gq" "gr" "gs" "gt" "gu" "gw" "gy" "hk" "hm" "hn" "hr" "ht" "hu" "id" "ie" "il" "im" "in" "info" "int" "io" "iq" "ir" "is" "it" "je" "jm" "jo" "jobs" "jp" "ke" "kg" "kh" "ki" "km" "kn" "kp" "kr" "kw" "ky" "kz" "la" "lb" "lc" "li" "lk" "lr" "ls" "lt" "lu" "lv" "ly" "ma" "mc" "md" "me" "mg" "mh" "mil" "mk" "ml" "mm" "mn" "mo" "mobi" "mp" "mq" "mr" "ms" "mt" "mu" "museum" "mv" "mw" "mx" "my" "mz" "na" "name" "nc" "ne" "net" "nf" "ng" "ni" "nl" "no" "np" "nr" "nu" "nz" "om" "org" "pa" "pe" "pf" "pg" "ph" "pk" "pl" "pm" "pn" "post" "pr" "pro" "ps" "pt" "pw" "py" "qa" "re" "ro" "rs" "ru" "rw" "sa" "sb" "sc" "sd" "se" "sg" "sh" "si" "sj" "sk" "sl" "sm" "sn" "so" "sr" "st" "su" "sv" "sx" "sy" "sz" "tc" "td" "tel" "tf" "tg" "th" "tj" "tk" "tl" "tm" "tn" "to" "tp" "tr" "travel" "tt" "tv" "tw" "tz" "ua" "ug" "uk" "us" "uy" "uz" "va" "vc" "ve" "vg" "vi" "vn" "vu" "wf" "ws" "xn" "xxx" "ye" "yt" "za" "zm" "zw"} tld)
+        domain))))
+
+(defn fetch-links [domain]
+  (info (str "fetching: " domain))
+  (try
+    (let [{:keys [body] :as response}
+          (http/get (str "http://" domain)
+                    connection-defaults)]
+      (map (comp :href :attrs)
+           (html/select
+            (html/html-resource (java.io.StringReader. body))
+            [:a])))
+    (catch Exception e
+      (error e)
+      nil)))
+
+(defn get-remote-domains [domain links]
+  (set
+   (remove #(or (nil? %) (= % domain))
+           (map extract-domain-from-url links))))
+
+(defn links [domain]
+  (let [crawl-delay (or (:crawls-delay (robots domain)) default-crawl-delay)
+        links (get-domain-value domain :links)]
+    (if (or (not links)
+            (time/before? (get-domain-value domain :last-parse-time)
+                          (time/minus (time/now) crawl-delay)))
+      (update-domain! domain :links
+                      (do
+                        (update-domain! domain :last-parse-time (time/now))
+                        (get-remote-domains domain (fetch-links domain))))
+      links)))
+
+(defn enqueue-urls [urls]
+  (doseq [url urls]
+    (when (and url
+               (not (get @domains url)))
+      (>!! uri-queue url))))
+
+(defn process-url [url]
+  (let [links (links url)]
+    (when links
+      (enqueue-urls links))))
+   
+(def running-threads (atom 0))
+
+(defn crawl-web [seed]
+  (onto-chan uri-queue seed false)
+  (doseq [thread-index (range 8)]
+    (thread (swap! running-threads inc)
+            (loop [url (<!! uri-queue)]
+              (when url
+                (process-url url)
+                (recur (<!! uri-queue))))
+            (swap! running-threads dec) 
+            (info (str "Download thread " thread-index " has ended.")))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
