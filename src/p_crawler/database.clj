7ffb49179481f98bd597fbdebd1b7f73869348c0
Andrew Stine <illuminati1113@gmail.com>
2014-10-31 19:12:04 -0400

    Start on database layer

--- src/p_crawler/database.clj
+++ src/p_crawler/database.clj
@@ -2,3 +2,30 @@
   (:require [monger.core :as mg]))
 
 (def db (mg/get-db (mg/connect) "p-crawler"))
+
+(def cache (atom {}))
+
+(defn add-collection! [collection]
+  (swap! cache assoc collection (atom {})))
+
+(defn remove-collection! [collection]
+  (swap! cache dissoc collection))
+
+(defn get-collection [collection]
+  (or (get @cache collection)
+      (add-collection! collection)))
+
+(defn add-document! [collection doc-name document]
+  (swap! (get-collection collection)
+         assoc doc-name document))
+
+(defn remove-document! [collection doc-name]
+  (swap! (get-collection collection)
+         dissoc doc-name))
+
+(defn get-document [collection doc-name]
+  (get (get-collection collection) doc-name))
+
+(defn update-document! [collection doc-name document]
+  (swap! (get-collection collection)
+         assoc doc-name document))
