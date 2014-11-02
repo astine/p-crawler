fdd1794f441a10ef0296134896774b34fecdb9c4
Andrew Stine <illuminati1113@gmail.com>
2014-11-02 17:57:17 -0500

    Some transducers so I don't have to upgrade to 1.7 just yet

---
+++ src/p_crawler/transducers.clj
@@ -0,0 +1,61 @@
+(ns p-crawler.transducers)
+
+
+(defn unreduced
+  "If x is reduced?, returns (deref x), else returns x"
+  {:added "1.7"}
+  [x]
+  (if (reduced? x) (deref x) x))
+
+(defn ^:private preserving-reduced
+  [rf]
+  #(let [ret (rf %1 %2)]
+     (if (reduced? ret)
+       (reduced ret)
+       ret)))
+
+(defn catting
+  "A transducer which concatenates the contents of each input, which must be a
+collection, into the reduction."
+  {:added "1.7"}
+  [rf]
+  (let [rrf (preserving-reduced rf)]
+    (fn
+      ([] (rf))
+      ([result] (rf result))
+      ([result input]
+         (reduce rrf result input)))))
+
+(defn partitioning-all [^long n]
+  (fn [rf]
+    (let [a (java.util.ArrayList. n)]
+      (fn
+        ([] (rf))
+        ([result]
+           (let [result (if (.isEmpty a)
+                          result
+                          (let [v (vec (.toArray a))]
+                            ;;clear first!
+                            (.clear a)
+                            (unreduced (rf result v))))]
+             (rf result)))
+        ([result input]
+           (.add a input)
+           (if (= n (.size a))
+             (let [v (vec (.toArray a))]
+               (.clear a)
+               (rf result v))
+             result))))))
+
+(defn mapping [f]
+  (fn [rf]
+    (fn
+      ([] (rf))
+      ([result] (rf result))
+      ([result input]
+         (rf result (f input)))
+      ([result input & inputs]
+         (rf result (apply f input inputs))))))
+
+(defn mapcatting [f]
+  (comp (mapping f) catting))
